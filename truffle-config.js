71647ee3139a6b3fccfd64c5db4071e8361fa5ae
Kevin Bluer <kevin@bluer.com>
2020-06-25 10:10:28 -0500

    init

---
+++ truffle-config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  networks: {
+    teams: {
+      url: `ws://marshmallow:8002/3c32ef32-6065-4d80-ae3e-3ee28b9f3007`,
+      network_id: "*",
+      gas: 5500000
+    }
+  },
+};
